e1cc50162af7350769ea975cc2c0da67c795c757
Shridipa <dhar.shridipa@gmail.com>
2026-02-28 10:25:18 +0530

    Update Backend URL to newest Cloudflare Tunnel

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,4 +2,4 @@
 // When running locally: http://localhost:8000
 // When deployed: Update this to your PRODUCTION backend domain (Render, Railway, etc.)
 
-export const API_BASE_URL = 'https://circles-edt-exceptions-twiki.trycloudflare.com';
+export const API_BASE_URL = 'https://relatives-opponents-cumulative-vital.trycloudflare.com';
